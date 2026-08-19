71dbbeb88df72335bfed8c7864e785f54cfb92a8
Ben-Butcher <benhtembe@gmail.com>
2026-08-19 19:21:15 +0200

    Made first card

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Element from "./components/Element.tsx";
+export default function App() {
+  return (
+    <div className="m-1.5">
+      <Element />
+    </div>
+  );
+}
---
+++ src/components/Element.tsx
@@ -0,0 +1,8 @@
+export default function Element() {
+  return (
+    <div className="bg-white-200 h-40 w-40 flex justify-center items-center flex-col rounded-2xl shadow-xl shadow-black">
+      <span className="">🪨</span>
+      <p className="text-lg font-medium">Rock</p>
+    </div>
+  );
+}
